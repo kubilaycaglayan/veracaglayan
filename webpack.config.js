0b0f838b020951256bce43d047892105544a3787
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-08-09 22:23:22 +0300

    Add Babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,16 @@ module.exports = {
         test: /\.html$/i,
         loader: 'html-loader',
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ],
   },
 };
